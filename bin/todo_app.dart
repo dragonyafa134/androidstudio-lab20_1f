f8d1b7556548749fc9e0c5bc2aed1f43b15ea96c
whypikmi <ttelatnikova1@yandex.ru>
2026-04-14 13:31:01 +0300

    add package ansicolor

--- bin/todo_app.dart
+++ bin/todo_app.dart
@@ -1,4 +1,35 @@
+import 'dart:io';
+import 'package:todo_app/todo.dart';
+import 'package:todo_app/todo_repositoru.dart';
+import 'package:ansicolor/ansicolor.dart';
+
+
+
+final AnsiPen greenPen = AnsiPen()..green();
+final AnsiPen redPen = AnsiPen()..red();
+final AnsiPen bluePen = AnsiPen()..blue();
+final AnsiPen yellowPen = AnsiPen()..yellow();
+
 void main() {
+  TodoRepositoru repo = TodoRepositoru();
+  printMenu();
+  while (true) {
+    stdout.write('> ');
+    String? input = stdin.readLineSync();
+    if (input == null) {
+      continue;
+    }
+    input = input.trim();
+    if (input.isEmpty) {
+      continue;
+    }
+    bool shouldExit = handleCommand(repo, input);
+
+    if (shouldExit) {
+      break;
+    }
+  }
+
   // print('Hello world');
   // String name = 'Руна';
   // String? name2 = null;
@@ -24,6 +55,91 @@ void main() {
 
   // printTodo(titile: "купить");
   // printTodo(titile: "сделать дз", done: true);
+}
+
+bool handleCommand(TodoRepositoru repo, String input) {
+  List<String> parts = input.split(' ');
+  String command = parts[0].toLowerCase();
+  try{
+    switch (command){
+      case "add":
+        addCommand(repo, input);
+        break;
+      case "list":
+        listCommand(repo);
+        break;
+      case "done":
+        doneCommand(repo, parts);
+        break;
+      case "delete" :
+        deleteCommand(repo, parts);
+        break;
+      case "exit":
+        print(bluePen("Выход из программы"));
+        return true;
+      default:
+        print("Неизвестная команда");
+    }
+  }
+  catch (e) {
+    print(redPen("Ошибка $e"));
+  }
+  return false;
+}
+
+void printMenu() {
+  print(yellowPen("Приложенеия туду"));
+  print('Команды');
+  print(' add <текст>      -добавить задачу');
+  print(' list             -показать список');
+  print(' done <id>        -отметить выполнения');
+  print(' delete <id>      -удалить задсчу');
+  print(' exit             -вызож');
+  print('');
+}
+
+void addCommand(TodoRepositoru repo, String input) {
+  if (input.length <= 4) {
+    print('Ошибка введите текст');
+    return;
+  }
+
+  String title = input.substring(4).trim();
+  repo.add(title);
+
+  print('Задача добавлена');
+}
+
+void listCommand(TodoRepositoru repo) {
+  List<Todo> todos = repo.getAll();
+  if (todos.isEmpty) {
+    print('список задач пуст');
+    return;
+  }
+  for (var todo in todos) {
+    print(todo);
+  }
+}
+
+void doneCommand (TodoRepositoru repo, List<String> parts){
+  if (parts.length < 2){
+    print("Ошибка укажити id");
+    return;
+  }
+  int id = int.parse(parts[1]);
+  repo.complete(id);
+  print("Задача отмечана выполненной");
+}
+
+
+void deleteCommand (TodoRepositoru repo,List<String> parts){
+  if (parts.length < 2){
+    print("Ошибка укажите id");
+    return;
+  }
+  int id = int.parse(parts[1]);
+  repo.delete(id);
+  print("задача удалена");
 }
 // void printTodo({required String titile, bool done = false}) {
 //   print('${done ? '+' : '-'} $titile');
